079e96b38a63858786052e77e67a1fb0462876b0
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2016-12-12 21:31:08 -0500

    part 1

--- day01/day01/day01/ViewController.m
+++ day01/day01/day01/ViewController.m
@@ -10,14 +10,95 @@
 
 @interface ViewController ()
 
+@property (nonatomic) NSNumberFormatter *numberFormatter;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self day1];
 }
 
+- (void)day1 {
+    NSString *input = [self inputAsArray][0];
+
+    NSArray<NSString *> *inputArray = [input componentsSeparatedByString:@", "];
+    CGPoint location = CGPointZero;
+    NSString *previousMove = @"";
+
+    for (NSString *command in inputArray) {
+        NSString *direction = [command substringToIndex:1];
+        NSString *distanceStr = [command stringByReplacingOccurrencesOfString:direction withString:@""];
+        NSNumber *distance = [self numberFromString:distanceStr];
+        
+        // First Move or Up
+        if ([previousMove isEqualToString:@""] || [previousMove isEqualToString:@"up"]) {
+            if ([direction isEqualToString:@"L"]) {
+                location.x -= distance.integerValue;
+                previousMove = @"left";
+            } else {
+                location.x += distance.integerValue;
+                previousMove = @"right";
+            }
+        }
+        // Right
+        else if ([previousMove isEqualToString:@"right"]) {
+            if ([direction isEqualToString:@"L"]) {
+                location.y += distance.integerValue;
+                previousMove = @"up";
+            } else {
+                location.y -= distance.integerValue;
+                previousMove = @"down";
+            }
+        }
+        // Left
+        else if ([previousMove isEqualToString:@"left"]) {
+            if ([direction isEqualToString:@"L"]) {
+                location.y -= distance.integerValue;
+                previousMove = @"down";
+            } else {
+                location.y += distance.integerValue;
+                previousMove = @"up";
+            }
+        }
+        // Down
+        else if ([previousMove isEqualToString:@"down"]) {
+            if ([direction isEqualToString:@"L"]) {
+                location.x += distance.integerValue;
+                previousMove = @"right";
+            } else {
+                location.x -= distance.integerValue;
+                previousMove = @"left";
+            }
+        }
+    }
+
+    NSLog(@"result: %f", fabs(location.x) + fabs(location.y));
+}
+
+#pragma mark - Common Helper Methods
+
+// NSString -> NSNumber
+- (NSNumber *)numberFromString:(NSString *)input {
+    if (!self.numberFormatter) {
+        self.numberFormatter = [[NSNumberFormatter alloc] init];
+        self.numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    }
+    return [self.numberFormatter numberFromString:input];
+}
+
+// txt --> NSArray<NSString *>
+- (NSArray<NSString *> *)inputAsArray {
+    NSString *filePath = @"input";
+    NSString *fileRoot = [[NSBundle mainBundle] pathForResource:filePath ofType:@"txt"];
+    NSString *fileContents = [NSString stringWithContentsOfFile:fileRoot
+                                                       encoding:NSUTF8StringEncoding error:nil];
+    NSMutableArray<NSString *> *allLinedStrings = [fileContents componentsSeparatedByCharactersInSet:
+                                                   [NSCharacterSet newlineCharacterSet]].mutableCopy;
+    [allLinedStrings removeLastObject];
+    return allLinedStrings.copy;
+}
 
 @end
