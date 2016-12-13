7e4f22182ef67e9ef20e39c8aa0765f02720f9d5
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2016-12-12 23:06:58 -0500

    day 5 initial commit

---
+++ day05/day05/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  day05
+//
+//  Created by David Weissler on 12/12/16.
+//  Copyright © 2016 David Weissler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
