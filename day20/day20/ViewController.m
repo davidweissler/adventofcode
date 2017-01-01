d63cefca8e31fba7c6d9f513bcf445f89bfa535c
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2016-12-31 23:44:29 -0500

    part 1

--- day20/day20/ViewController.m
+++ day20/day20/ViewController.m
@@ -8,7 +8,18 @@
 
 #import "ViewController.h"
 
+@interface Pair : NSObject
+@property (nonatomic) NSUInteger low;
+@property (nonatomic) NSUInteger high;
+@end
+
+@implementation Pair
+@end
+
+
 @interface ViewController ()
+
+@property (nonatomic) NSNumberFormatter *numberFormatter;
 
 @end
 
@@ -16,14 +27,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self day20];
+}
+
+- (void)day20 {
+    NSArray<NSString *> *input = [self inputAsArray];
+    input = [input sortedArrayUsingComparator:^NSComparisonResult(NSString  * _Nonnull obj1, NSString  * _Nonnull obj2) {
+        NSArray<NSString *> *objArray1 = [obj1 componentsSeparatedByString:@"-"];
+        NSArray<NSString *> *objArray2 = [obj2 componentsSeparatedByString:@"-"];
+        
+        NSNumber *num1 = [self numberFromString:objArray1[0]];
+        NSNumber *num2 = [self numberFromString:objArray2[0]];
+        
+        if (num1.unsignedIntegerValue < num2.unsignedIntegerValue) {
+            return NSOrderedAscending;
+        } else {
+            return NSOrderedDescending;
+        }
+    }];
+
+    NSMutableArray<Pair *> *merged = @[].mutableCopy;
+    for (NSString *inputLine in input) {
+        NSArray<NSString *> *lineArray = [inputLine componentsSeparatedByString:@"-"];
+        Pair *currentPair = [Pair new];
+        currentPair.low = [[self numberFromString:lineArray[0]] unsignedIntegerValue];
+        currentPair.high = [[self numberFromString:lineArray[1]] unsignedIntegerValue];
+        
+        Pair *lastPair = merged.lastObject;
+        if (!lastPair) {
+            [merged addObject:currentPair];
+            continue;
+        }
+        
+        if (currentPair.low > lastPair.high + 1) {
+            // [merged addObject:currentPair];
+            NSLog(@"lowest free IP: %lu", lastPair.high + 1);
+            break;
+        } else {
+            if (currentPair.high > lastPair.high) {
+                lastPair.high = currentPair.high;
+            }
+        }
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Common Helper Methods
+
+// NSString -> NSNumber
+- (NSNumber *)numberFromString:(NSString *)input {
+    if (!self.numberFormatter) {
+        self.numberFormatter = [[NSNumberFormatter alloc] init];
+        self.numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    }
+    return [self.numberFormatter numberFromString:input];
 }
 
-
+// txt --> NSArray<NSString *>
+- (NSArray<NSString *> *)inputAsArray {
+    NSString *filePath = @"input";
+    NSString *fileRoot = [[NSBundle mainBundle] pathForResource:filePath ofType:@"txt"];
+    NSString *fileContents = [NSString stringWithContentsOfFile:fileRoot
+                                                       encoding:NSUTF8StringEncoding error:nil];
+    NSMutableArray<NSString *> *allLinedStrings = [fileContents componentsSeparatedByCharactersInSet:
+                                                   [NSCharacterSet newlineCharacterSet]].mutableCopy;
+    [allLinedStrings removeLastObject];
+    return allLinedStrings.copy;
+}
 @end
