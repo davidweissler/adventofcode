4d9e558b47c03253129649e5bde053da4c598c36
David Weissler <davidweissler@Davids-MBP.fios-router.home>
2017-01-02 21:32:45 -0500

    initial commit

---
+++ day22/day22/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day22
+//
+//  Created by David Weissler on 1/2/17.
+//  Copyright © 2017 David Weissler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
