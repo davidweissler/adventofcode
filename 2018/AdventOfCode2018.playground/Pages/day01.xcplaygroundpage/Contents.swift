deb0916716d61946255619ef58645f278ad40e05
David <david.weissler@gmail.com>
2018-12-01 13:03:31 -0500

    day 1 part 2

--- 2018/AdventOfCode2018.playground/Pages/day01.xcplaygroundpage/Contents.swift
+++ 2018/AdventOfCode2018.playground/Pages/day01.xcplaygroundpage/Contents.swift
@@ -20,8 +20,27 @@ func processInput1(input: inout String) {
     print(inputArr)
 }
 
+func processInput2(input: inout String) {
+    let inputArr = input.components(separatedBy: "\n").map {  return Int($0) ?? 0 }
+    var seenFreq = [Int: Int]()
+    seenFreq[0] = 1
+    var currFreq = 0
+    var foundTwice = false
+    while (!foundTwice) {
+        for num in inputArr {
+            currFreq += num
+            if seenFreq[currFreq] == 1 {
+                foundTwice = true
+                break
+            }
+            seenFreq[currFreq, default:0] += 1
+        }
+    }
+    print(currFreq)
+}
+
 
 if var input = readInput() {
-    processInput1(input: &input)
+    processInput2(input: &input)
 }
 
