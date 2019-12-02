13251af76a7e02b1ba40bed47e2c66354f8983af
davidweissler <david.weissler@gmail.com>
2019-12-02 13:21:46 -0500

    day 2.1

--- 2019/AdventOfCode2019.playground/Pages/day02.xcplaygroundpage/Contents.swift
+++ 2019/AdventOfCode2019.playground/Pages/day02.xcplaygroundpage/Contents.swift
@@ -13,9 +13,34 @@ func readInput() -> String? {
 }
 
 func processInput1(_ input: String) {
-    let inputArr = input.components(separatedBy: "\n")
-        .map { (Int($0) }
-    print(inputArr)
+    var intArr = input.components(separatedBy: ",")
+        .map { Int($0) ?? 0 }
+
+    intArr[1] = 12
+    intArr[2] = 2
+    for i in stride(from: 0, to: intArr.count, by: 4) {
+        let currentVal = intArr[i]
+        if currentVal == 99 {
+            print(intArr[0])
+            return
+        }
+
+        let pos1 = intArr[i + 1]
+        let pos2 = intArr[i + 2]
+        let pos3 = intArr[i + 3]
+        let val1 = intArr[pos1]
+        let val2 = intArr[pos2]
+
+        if currentVal == 1 {
+            intArr[pos3] = val1 + val2
+            continue
+        }
+        
+        if currentVal == 2 {
+            intArr[pos3] = val1 * val2
+            continue
+        }
+    }
 }
 
 if let input = readInput() {
