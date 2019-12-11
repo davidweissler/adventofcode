65f0578f70ec896a21a2994407c57ea8e26a28d8
davidweissler <david.weissler@gmail.com>
2019-12-10 22:03:48 -0500

    day 4 part 2

--- 2019/AdventOfCode2019.playground/Pages/day04.xcplaygroundpage/Contents.swift
+++ 2019/AdventOfCode2019.playground/Pages/day04.xcplaygroundpage/Contents.swift
@@ -31,6 +31,42 @@ func part1(lower: Int, upper: Int) -> Int {
     return possiblePasswords
 }
 
+func part2(lower: Int, upper: Int) -> Int {
+    var possiblePasswords = 0
+    
+    for num in lower...upper {
+        let numArr = String(num).map { Int(String($0))! }
+        
+        var matchingDigitCount = 1
+        var hasDouble = false
+        var isDecreasing = false
+        var prevNum = -1
+        for digit in numArr {
+            if digit < prevNum {
+                isDecreasing = true
+            }
+            if isDecreasing { break }
+
+            if !hasDouble {
+                if digit == prevNum {
+                    matchingDigitCount += 1
+                } else {
+                    if matchingDigitCount == 2 {
+                        hasDouble = true
+                    }
+                    matchingDigitCount = 1
+                }
+            }
+            prevNum = digit
+        }
+        
+        if (hasDouble || matchingDigitCount == 2) && !isDecreasing {
+            possiblePasswords += 1
+        }
+    }
+    return possiblePasswords
+}
+
 let lowerBounds = 128392
 let upperBounds = 643281
-part1(lower: lowerBounds, upper: upperBounds)
+print(part2(lower: lowerBounds, upper: upperBounds))
