a6113ed6ca12a17059a7ab8a846b255b2787798e
David Weissler <david.weissler@gmail.com>
2020-12-05 09:24:22 -0500

    2020 initial commit

---
+++ 2020/AdventOfCode2019 copy.playground/Pages/day01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Parsing funcs
+
+func readInput() -> String? {
+    let fileURL = Bundle.main.url(forResource: "Input", withExtension: "txt")
+    let content = try? String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+    guard var input = content else { return nil }
+    input.removeLast()
+    return input
+}
+
+func processInput1(_ input: String) {
+    let inputArr = input.components(separatedBy: "\n")
+    print(inputArr)
+}
+
+
+if let input = readInput() {
+    processInput1(input)
+}
+
