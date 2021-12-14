1036a15aebb7a894b639aafab8ef4b220f71e297
davidweissler <david.weissler@gmail.com>
2021-12-14 16:48:55 -0500

    day 3.2

--- 2021/AdventOfCode2021.playground/Pages/day03.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day03.xcplaygroundpage/Contents.swift
@@ -38,8 +38,58 @@ func processInput1(_ input: String) {
   print("\(gammaInt) x \(epsilonInt) = \(gammaInt * epsilonInt)")  
 }
 
+func processInput2(_ input: String) {
+  let inputArr: [[Int]] = input.components(separatedBy: "\n").map { str in
+    var output = [Int]()
+    for i in str {
+      output.append(Int(String(i))!)
+    }
+    return output
+  }
+
+  var oxygenInput = inputArr
+  var i = 0
+  while oxygenInput.count > 1 {
+    let filterNumber: Int
+    let onesCount = oxygenInput.filter { $0[i] == 1 }
+    let zerosCount = oxygenInput.filter { $0[i] == 0 }
+    if onesCount.count >= zerosCount.count {
+      filterNumber = 1
+    } else {
+      filterNumber = 0
+    }
+    oxygenInput = oxygenInput.filter { $0[i] == filterNumber }
+    i += 1
+  }
+  
+  var carbonInput = inputArr
+  i = 0
+  while carbonInput.count > 1 {
+    let filterNumber: Int
+    let onesCount = carbonInput.filter { $0[i] == 1 }
+    let zerosCount = carbonInput.filter { $0[i] == 0 }
+    if onesCount.count < zerosCount.count {
+      filterNumber = 1
+    } else {
+      filterNumber = 0
+    }
+    carbonInput = carbonInput.filter { $0[i] == filterNumber }
+    i += 1
+  }
+
+  
+  print(oxygenInput)
+  print(carbonInput)
+  let oxygenString = oxygenInput.first!.reduce("") { partialResult, val in partialResult.appending("\(val)") }
+  let carbonString = carbonInput.first!.reduce("") { partialResult, val in partialResult.appending("\(val)") }
+  
+  let oxygenInt = Int(oxygenString, radix: 2)!
+  let carbonInt = Int(carbonString, radix: 2)!
+  print("\(oxygenInt) x \(carbonInt)  = \(oxygenInt * carbonInt)")
+}
+
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
 
 
