2e3fb773de065973ad80730126edb43ccc3dfb9e
davidweissler <david.weissler@gmail.com>
2021-12-19 17:40:10 -0500

    day 6.2

--- 2021/AdventOfCode2021.playground/Pages/day06.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day06.xcplaygroundpage/Contents.swift
@@ -41,6 +41,39 @@ func processInput1(_ input: String) {
   print(total)
 }
 
+func processInput2(_ input: String) {
+  let inputArr = input.split(separator: ",").map { Int($0)! }
+  let fishArr = inputArr.map { Int($0) }
+  
+  var fishCache = [Int: Int]()
+  
+  for i in fishArr {
+    fishCache[i, default: 0] += 1
+  }
+  print(fishCache)
+  
+  for _ in 1...256 {
+    for i in 0...8 {
+      if fishCache.keys.contains(i) {
+        fishCache[i - 1]  = fishCache[i]
+        fishCache.removeValue(forKey: i)
+      }
+    }
+    // Move -1 -> 6
+    let birthCount: Int = fishCache.removeValue(forKey: -1) ?? 0
+    if birthCount > 0 {
+      fishCache[8] = birthCount
+      fishCache[6, default: 0] += birthCount
+    }
+  }
+  var total = 0
+  for (_,v) in fishCache.enumerated() {
+    total += v.value
+  }
+  print(fishCache)
+  print(total)
+}
+
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
