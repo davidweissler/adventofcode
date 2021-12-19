380c57449935a0b7e97f4eaa65a125fef4a4578c
davidweissler <david.weissler@gmail.com>
2021-12-19 18:23:41 -0500

    day 7.2

--- 2021/AdventOfCode2021.playground/Pages/day07.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day07.xcplaygroundpage/Contents.swift
@@ -25,6 +25,29 @@ func processInput1(_ input: String) {
   print(totalGas.min() ?? -1)
 }
 
+func processInput2(_ input: String) {
+  let inputArr = input.split(separator: ",").map { Int($0)! }
+  let posArr = inputArr.map { Int($0) }
+  let largest = posArr.max() ?? 0
+  var totalGas = [Int]()
+  var gasCache = [Int: Int]()
+  for i in 0...largest {
+    let changed = posArr.map { val -> Int in
+      let diff = abs(val - i)
+      if let saved = gasCache[diff] {
+        return saved
+      }
+      let toSave = (0...diff).reduce(0, +)
+      gasCache[diff] = toSave
+      return toSave
+    }
+    let sum = changed.reduce(0, +)
+    totalGas.append(sum)
+  }
+  print(totalGas)
+  print(totalGas.min() ?? -1)
+}
+
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
