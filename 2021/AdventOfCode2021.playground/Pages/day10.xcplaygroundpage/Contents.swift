ad0e579440d33e90e7f2b8a98273426b1802e959
davidweissler <david.weissler@gmail.com>
2021-12-21 14:47:17 -0500

    day 10.2

--- 2021/AdventOfCode2021.playground/Pages/day10.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day10.xcplaygroundpage/Contents.swift
@@ -69,6 +69,59 @@ func processInput1(_ input: String) {
   print(score)
 }
 
+func processInput2(_ input: String) {
+  let inputArr = input.split(separator: "\n")
+  let subsystems = inputArr.map { $0.map { String($0) }  }
+  let open: Set<String> = ["(", "[", "{", "<"]
+  
+  var completions = [[String]]()
+  
+  subsystems.forEach { subsystem in
+    var i = 0
+    var stack = [String]()
+    
+    var shouldContinue = true
+    var isCorrupted = false
+    while i < subsystem.count && shouldContinue {
+      let curr = subsystem[i]
+      if open.contains(curr) {
+        stack.insert(curr, at: 0)
+        i += 1
+        continue
+      }
+
+      let top = stack.remove(at: 0)
+      if match(curr) != top {
+        // wrong closing
+        shouldContinue = false
+        isCorrupted = true
+        continue
+      }
+      
+      i += 1
+    }
+    
+    if !isCorrupted && !stack.isEmpty {
+      let closing = stack.map { match($0) }
+      completions.append(closing)
+    }
+  }
+  
+  let points = [")": 1, "]": 2, "}": 3, ">": 4]
+  var scores = [Int]()
+  completions.forEach { completion in
+    var total = 0
+    completion.forEach { curr in
+      total *= 5
+      total += points[curr]!
+    }
+    scores.append(total)
+  }
+  scores.sort()
+  let middle = scores.count / 2
+  print(scores[middle])
+}
+
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
