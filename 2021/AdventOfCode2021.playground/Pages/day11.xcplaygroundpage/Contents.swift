25ea8515b0e574f07030f4d4e5969b62f3395e97
david <david.weissler@gmail.com>
2021-12-21 22:25:54 -0500

    day 11.2

--- 2021/AdventOfCode2021.playground/Pages/day11.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day11.xcplaygroundpage/Contents.swift
@@ -57,6 +57,56 @@ func processInput1(_ input: String) {
   print(totalFlashes)
 }
 
+func processInput2(_ input: String) {
+  let inputArr = input.split(separator: "\n")
+  var energyMap = [[Int]]()
+  inputArr.forEach { row in
+    energyMap.append(row.map { Int(String($0))! })
+  }
+
+  for step in 1...Int.max {
+    var willFlash = [(Int, Int)]()
+    // incremement every cell
+    for row in 0..<energyMap.count {
+      for col in 0..<energyMap[row].count {
+        energyMap[row][col] += 1
+        if energyMap[row][col] > 9 {
+          willFlash.append((row, col))
+        }
+      }
+    }
+    
+    var hasFlashed = [(Int, Int)]()
+    while !willFlash.isEmpty {
+      let currentFlash = willFlash.removeFirst()
+      hasFlashed.append(currentFlash)
+      energyMap = incrementNeighbors(map: energyMap, row: currentFlash.0, col: currentFlash.1)
+      for row in 0..<energyMap.count {
+        for col in 0..<energyMap[row].count {
+          if energyMap[row][col] <= 9 {
+            continue
+          }
+          if !hasFlashed.contains(where: { hf in hf.0 == row && hf.1 == col}) && !willFlash.contains(where: { hf in hf.0 == row && hf.1 == col}) {
+            willFlash.append((row, col))
+          }
+        }
+      }
+    }
+    //print(hasFlashed)
+    hasFlashed.forEach {
+      energyMap[$0.0][$0.1] = 0
+    }
+    if hasFlashed.count == energyMap.count * energyMap[0].count {
+      print("all flashed: step \(step)")
+      break
+    }
+  }
+
+  for row in energyMap {
+    print(row)
+  }
+}
+
 func incrementNeighbors(map: [[Int]], row: Int, col: Int) -> [[Int]] {
   var newMap = map
   // upper left
@@ -136,5 +186,5 @@ func incrementNeighbors(map: [[Int]], row: Int, col: Int) -> [[Int]] {
 }
 
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
