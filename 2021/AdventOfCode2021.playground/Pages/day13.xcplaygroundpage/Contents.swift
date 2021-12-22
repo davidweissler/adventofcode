a60f74c98b0331b9a42623501e0b7abf432a023c
davidweissler <david.weissler@gmail.com>
2021-12-22 14:50:59 -0500

    day 13.2

--- 2021/AdventOfCode2021.playground/Pages/day13.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/day13.xcplaygroundpage/Contents.swift
@@ -71,7 +71,73 @@ func processInput1(_ input: String) {
   print(coordinatesSet.count)
 }
 
+func processInput2(_ input: String) {
+  let inputArr = input.split(separator: "\n")
+  print(inputArr)
+  var instructions = [String]()
+  var coordinates = [Coordinate]()
+  inputArr.forEach { line in
+    if line.contains("fold") {
+      instructions.append(String(line))
+    } else {
+      let coordStr = line.components(separatedBy: ",").map { Int($0)! }
+      coordinates.append(Coordinate(row: coordStr[1], col: coordStr[0]))
+    }
+  }
+  
+  print(coordinates)
+  print(instructions)
+  var coordinatesSet = Set(coordinates)
+  // Get first instruction
+  let endCount = instructions.count
+  for i in 0..<endCount {
+    var instruction = instructions[i]
+    let instructionArr = instruction.components(separatedBy: " ")
+    guard let whereToFold = instructionArr.last else {
+      fatalError("expeted row")
+    }
+    
+    let whereToFoldSplit = whereToFold.components(separatedBy: "=")
+    let toRemove: Set<Coordinate>
+    let splitVal = Int(whereToFoldSplit[1])!
+    if whereToFoldSplit[0] == "y" {
+      // Find all coordinates where row > splitVal
+      toRemove = coordinatesSet.filter { $0.row > splitVal }
+    } else {
+      // Final all coordinates where col > splitVal
+      toRemove = coordinatesSet.filter { $0.col > splitVal }
+      
+    }
+    // Remove each coord beyond the split from the coordinate set
+    toRemove.forEach { coordinatesSet.remove($0) }
+    
+    // Map the new coordinates
+    let newCoords = toRemove.map { coord -> Coordinate in
+      if whereToFoldSplit[0] == "y" {
+        var newRow = coord.row - splitVal
+        newRow = splitVal - newRow
+        return Coordinate(row: newRow , col: coord.col)
+      } else {
+        var newCol = coord.col - splitVal
+        newCol = splitVal - newCol
+        return Coordinate(row: coord.row , col: newCol)
+      }
+    }
+    newCoords.forEach { coordinatesSet.insert($0) }
+  }
+  print(coordinatesSet.count)
+  var maxRow = 0
+  var maxCol = 0
+  coordinatesSet.forEach {
+    maxCol = max(maxCol, $0.col)
+    maxRow = max(maxRow, $0.row)
+  }
+  
+  var grid: [[String]] = Array(repeating: Array(repeating: " ", count: maxCol + 1), count: maxRow + 1)
+  coordinatesSet.forEach { grid[$0.row][$0.col] = "#" }
+  grid.forEach { print($0) }
+}
 
 if let input = readInput() {
-  processInput1(input)
+  processInput2(input)
 }
